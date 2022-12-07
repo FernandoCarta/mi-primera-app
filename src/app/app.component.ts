fd2fe0108a79013137d8ac95cb96e7f0c022a3cf
FernandoCarta <desarrollo_cenati@koncilia.com.co>
2022-12-07 07:08:15 -0500

    Ajustes en appComponentHtml  y en appComponentTs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',//Nombre del componente al que vamos a hacer referencia en los archivos HTML
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
